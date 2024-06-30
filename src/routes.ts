5c75903a7501c16a233ddd14195ef40c7d77b06e
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2024-06-30 20:25:35 -0300

    feat: signin feature

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import { UserController } from './controllers/UserController'
 const routes = Router()
 
 routes.post('/signup', new UserController().signup)
+routes.post('/signin', new UserController().signin)
 
 export default routes
--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "../repositories/UserRepository"
-import { hashSync } from 'bcrypt'
-
+import { hashSync, compareSync } from 'bcrypt'
+import { TokenExpiredError, sign } from 'jsonwebtoken'
+import { userInfo } from "os"
 export class UserService {
     async signup(name: string, email: string, password: string) {
         try{
@@ -16,6 +17,33 @@ export class UserService {
             const hashPassword = hashSync(password, saltRounds);
 
             await userRepository.signup(name, email, hashPassword)
+        } catch(err) {
+            throw err
+        }
+    }
+
+    async signin(email:string, password:string) {
+        try{
+            if (!email) throw 'You must send an email'
+            if (!password) throw 'You must send a password'
+            
+            const userRepository = new UserRepository()
+            const user = await userRepository.findUserByEmail(email)
+            
+            if (!user) throw 'Wrong email or password'
+
+            const comparison = compareSync(password, user.password)
+            if (!comparison) throw 'Wrong email or password'
+
+            const token = sign({ id: user.id }, process.env.JWT_PASS ?? '', {
+                expiresIn: '8h',
+            })
+
+            const {password:_, ...userSession } = user
+            return {
+                user: userSession,
+                token
+            }
         } catch(err) {
             throw err
         }

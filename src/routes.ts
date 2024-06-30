81c978aea39f90db7042d5302f06fb49d42159f4
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2024-06-30 16:42:00 -0300

    chore: change http method

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,6 @@ import { UserController } from './controllers/UserController'
 
 const routes = Router()
 
-routes.use('/signup', new UserController().signup)
+routes.post('/signup', new UserController().signup)
 
 export default routes

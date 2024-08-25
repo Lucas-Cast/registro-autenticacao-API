6153a1fcf3241556acdfb0df94cf19db067972df
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2024-08-24 22:50:58 -0300

    fix: handle cors

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
-import { Router } from 'express'
-import { UserController } from './controllers/UserController'
+import { Router } from "express";
+import { UserController } from "./controllers/UserController";
+import cors from "cors";
 
-const routes = Router()
+const routes = Router();
+routes.use(cors());
+routes.post("/signup", new UserController().signup);
+routes.post("/signin", new UserController().signin);
+routes.delete("/deleteUser", new UserController().delete);
 
-routes.post('/signup', new UserController().signup)
-routes.post('/signin', new UserController().signin)
-routes.delete('/deleteUser', new UserController().delete)
-
-export default routes
+export default routes;
